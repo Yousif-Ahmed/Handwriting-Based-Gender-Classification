268da84fe4d198b6e78e15039668b524fbf8c192
Donia Esawi <14712018101273@stud.cu.edu.eg>
2022-05-29 20:46:41 +0200

    refactor:

--- helpers.py
+++ helpers.py
@@ -5,8 +5,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 from skimage.feature import hog, local_binary_pattern
-from sklearn.svm import LinearSVC
-
+from sklearn.svm import LinearSVC, SVC
+from sklearn.model_selection import GridSearchCV
 
 lables = ["Males", "Females"]
 
@@ -22,9 +22,10 @@ def read_data(filename):
         print(str(class_lable) + "start ..")
         for imagename in os.listdir(path):
             try:
-                img = cv2.imread(os.path.join(path, imagename), cv2.IMREAD_GRAYSCALE)
+                img = cv2.imread(os.path.join(path, imagename))
+                greyImg= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                 # we might need to reshape image here
-                data.append([img, class_lable])
+                data.append([greyImg, class_lable])
             except Exception as e:
                 print(e)
 
@@ -42,12 +43,10 @@ def hist( lbp):
   
 ##########################################
 
-def resized_images(data):
+def resized_images(data, width, height):
     out=[]
     for idx, img in enumerate(data):
 
-        width = 64
-        height = 128
         dim = (width, height)
 
         # data[idx] = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -55,3 +54,28 @@ def resized_images(data):
         resized = cv2.resize(data[idx], dim, interpolation=cv2.INTER_AREA)
         out.append(resized)
     return out
+
+
+##########################################
+def LBPfeatures(images, radius, pointNum):
+    hist_LBP = []
+    for img in images:
+        # print(img)
+        if type(pointNum) == int and type(radius) == int:
+            lbp = local_binary_pattern(img, int(pointNum), int(radius), method="uniform")
+            (hist, _) = np.histogram(lbp.ravel(), bins=range(0, pointNum + 3), range=(0, pointNum + 2))
+            hist_LBP.append(hist)
+    return hist_LBP
+
+    #########################################
+
+def GridSearch_tuning(X_train, y_train):
+    param_grid = {'C': [0.1, 1, 10, 100], 'gamma': [1, 0.1, 0.01, 0.001, 0.00001, 10]}
+    # Make grid search classifier
+    clf_grid = GridSearchCV(SVC(kernel="rbf"), param_grid, verbose=1)
+    # Train the classifier
+    clf_grid.fit(X_train, y_train)
+    # extract the parameters
+    best_params = clf_grid.best_params_
+    # return the parameters
+    return best_params
